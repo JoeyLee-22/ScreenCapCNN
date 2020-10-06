e22e9fc8fa26e153f27414fc0ed1cf008763b2c2
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-06 14:47:14 -0700

    put screenshot into images folder

--- screen_cap.py
+++ screen_cap.py
@@ -6,6 +6,6 @@ import pyautogui
 def start(cnn_instance, minutes=5):
     while(True):
         image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR) 
-        cv2.imwrite("screenshot.jpg", image)
+        cv2.imwrite("images/screenshot.jpg", image)
         cnn_instance.test(image.flatten())
         time.sleep(minutes*60)
