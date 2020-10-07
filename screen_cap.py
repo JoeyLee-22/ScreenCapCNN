3276ed6e7b06cacbdaad7187291204c5132d7468
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-06 21:41:29 -0700

    call classify and add timer for when next screenshot is taken

--- screen_cap.py
+++ screen_cap.py
@@ -5,7 +5,11 @@ import pyautogui
 
 def start(cnn_instance, minutes=5):
     while(True):
-        image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR) 
-        cv2.imwrite("images/screenshot.jpg", image)
-        cnn_instance.test(image.flatten())
-        time.sleep(minutes*60)
+        og_image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
+        image = cnn_instance.resize(og_image, 480)
+        print(cnn_instance.classify(np.array([image])))
+        cv2.imwrite("images/screenshot.jpg", og_image)
+        
+        for secs in range (int(minutes*60)):
+            time.sleep(0.98)
+            print(str(int(minutes*60)-int(secs)) + "/" + str(int(minutes*60)), end='\r')
