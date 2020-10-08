e880eb314de85d11c35cb324f2fa405361be6d95
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-07 19:20:12 -0700

    get rid of class, only need function

--- resize.py
+++ resize.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-class resize:
-    def resize(image, new_height, new_width):
-        new = np.empty([new_height,new_width,3])
-        factor = image.shape[0]/new_height
+def resize(image, new_height, new_width):
+    new = np.empty([new_height,new_width,3])
+    factor = image.shape[0]/new_height
 
-        for row in range (image.shape[0]):
-            for column in range (image.shape[1]):
-                if row%factor == 0 and column%factor == 0:
-                    new[int(row/factor)][int(column/factor)] = image[row][column]
+    for row in range (image.shape[0]):
+        for column in range (image.shape[1]):
+            if row%factor == 0 and column%factor == 0:
+                new[int(row/factor)][int(column/factor)] = image[row][column]
 
-        return new
+    return new
