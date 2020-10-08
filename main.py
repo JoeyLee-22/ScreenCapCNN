3d35767ea537ed593d6722baafd16915ca8afada
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-07 19:18:06 -0700

    use new_height and new_width instead of resize_factor for resize()

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from screen_cap import start
 from CNN import convolutional_neural_network
 
+new_height,new_width = 300,480
+
 cnn = convolutional_neural_network()
-cnn.data_prep(resize_factor=1/7)
+cnn.data_prep(new_height, new_width)
 cnn.run(epochs=25)
 
-start(cnn, minutes=0.1)
+start(cnn, new_height, new_width, minutes=0.1)
