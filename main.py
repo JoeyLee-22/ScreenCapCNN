e97c56a755bedf4862fd9e5f74880b5a5b62b098
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-07 20:44:22 -0700

    comment out certain function calls

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from CNN import convolutional_neural_network
 new_height,new_width = 300,480
 
 cnn = convolutional_neural_network()
-cnn.data_prep(new_height, new_width)
+cnn.data_prep(new_height,new_width)
+# cnn.data_prep_old(new_height,new_width)
 cnn.run(epochs=25)
 
-start(cnn, new_height, new_width, minutes=0.1)
+# start(cnn,new_height,new_width,minutes=0.1)
