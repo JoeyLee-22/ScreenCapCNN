e3f32304d25040d1ec4504755660e8822738bace
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 16:23:59 -0700

    add tqdm progress bar

--- data_prep.py
+++ data_prep.py
@@ -2,23 +2,32 @@ import os
 import pickle
 import matplotlib.image as mpimg
 import numpy as np
+from tqdm import tqdm
 from resize import resize
 
 def data_preparation(new_height, new_width):
     train_images = np.empty([len(os.listdir('train_images')),new_height,new_width,3])
     test_images = np.empty([len(os.listdir('test_images')),new_height,new_width,3])
 
+    print('\nPREPPING TRAIN IMAGES')
+    pbar = tqdm(total=len(os.listdir('train_images')))
     counter=0
     for filename in os.listdir('train_images'):
         img = mpimg.imread('train_images/%s' % filename)[:,:,:-1]
         train_images[counter] = resize(img, new_height, new_width)
+        pbar.update(1)
         counter+=1
+    pbar.close()
 
+    print('PREPPING TEST IMAGES')
+    pbar = tqdm(total=len(os.listdir('test_images')))
     counter=0
     for filename in os.listdir('test_images'):
         img = mpimg.imread('test_images/%s' % filename)[:,:,:-1]
         test_images[counter] = resize(img, new_height, new_width)
+        pbar.update(1)
         counter+=1
+    pbar.close()
 
     pickle.dump(train_images, open('dataset/train_images.pckl', 'wb'))
     pickle.dump(test_images, open('dataset/test_images.pckl', 'wb'))
