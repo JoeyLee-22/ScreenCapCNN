cb8db99e7fe48885d4f244aa6ba06a0293ae72ec
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 00:07:35 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from screen_cap import start
+from data_prep import data_prep
 from CNN import convolutional_neural_network
 
-new_height,new_width = 300,480
+(new_height,new_width) = (300,480)
 
+data_prep(new_height, new_width)
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.data_prep()
 cnn.run(epochs=25, train=True, plot=False)
 
-start(cnn,new_height,new_width,minutes=0.1)
+start(cnn, new_height, new_width, minutes=0.1)
