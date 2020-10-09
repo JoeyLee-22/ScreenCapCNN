684bb897c045da2e9fc1ff97328603cf8968b90e
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 15:51:43 -0700

    move data_prop call to CNN.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from screen_cap import start
-from data_prep import data_prep
 from CNN import convolutional_neural_network
 
 (new_height,new_width) = (300,480)
 
-data_prep(new_height, new_width)
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=25, train=True, plot=False)
+cnn.run(epochs=50, train=True, plot=False, data_prep=False)
 
 start(cnn, new_height, new_width, minutes=0.1)
