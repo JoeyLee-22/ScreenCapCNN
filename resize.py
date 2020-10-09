2993c5d8d2f3c3ad6c401cde3ae341c097965e1f
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-08 20:01:25 -0700

    update to make it faster

--- resize.py
+++ resize.py
@@ -2,11 +2,12 @@ import numpy as np
 
 def resize(image, new_height, new_width):
     new = np.empty([new_height,new_width,3])
-    factor = image.shape[0]/new_height
 
-    for row in range (image.shape[0]):
-        for column in range (image.shape[1]):
-            if row%factor == 0 and column%factor == 0:
-                new[int(row/factor)][int(column/factor)] = image[row][column]
+    row_factor = int(image.shape[0]/new_height)
+    column_factor = int(image.shape[1]/new_width)
+
+    for row in range (0,image.shape[0], row_factor):
+        for column in range (0,image.shape[1], column_factor):
+            new[int(row/row_factor)][int(column/column_factor)] = image[row][column]
 
     return new
