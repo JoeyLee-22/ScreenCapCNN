ea54676e317ec656fbdfe7dc06fafd110ce32e72
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-08 21:42:10 -0700

    improve running time

--- resize.py
+++ resize.py
@@ -6,8 +6,8 @@ def resize(image, new_height, new_width):
     row_factor = int(image.shape[0]/new_height)
     column_factor = int(image.shape[1]/new_width)
 
-    for row in range (0,image.shape[0], row_factor):
-        for column in range (0,image.shape[1], column_factor):
-            new[int(row/row_factor)][int(column/column_factor)] = image[row][column]
+    for row in range (new_height):
+        for column in range (new_width):
+            new[row][column] = image[row*row_factor][column*column_factor]
 
     return new
