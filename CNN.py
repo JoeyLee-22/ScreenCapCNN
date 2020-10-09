b76a701786b1efe66e03aa7da4d5130e24986e57
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 00:09:33 -0700

    add load_data function

--- CNN.py
+++ CNN.py
@@ -1,97 +1,79 @@
+# pyright: reportUnboundVariable=false
+
+import pickle
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 from keras.utils import to_categorical
+from dataset.test_labels import y_test
+from dataset.train_labels import y_train
 
 import main
 
 class convolutional_neural_network():
+    def __init__(self, new_height, new_width):
+        self.new_height = new_height
+        self.new_width = new_width
+
     def classify(self, image):
         return np.argmax(self.model.predict(image))
 
-    def resize(self, image):
-        new_height = int(image.shape[0]*self.resize_factor)
-        new_width = int(image.shape[1]*self.resize_factor)
+    def load_data(self):
+        train_images = pickle.load(open('dataset/train_images.pckl', 'rb'))
+        test_images = pickle.load(open('dataset/test_images.pckl', 'rb'))
 
-        new = np.empty([new_height,new_width,3])
-        factor = image.shape[0]/new_height
+        train_labels = to_categorical(y_train)
+        test_labels = to_categorical(y_test)
 
-        for row in range (image.shape[0]):
-            for column in range (image.shape[1]):
-                if row%factor == 0 and column%factor == 0:
-                    new[int(row/factor)][int(column/factor)] = image[row][column]
+        return (train_images,train_labels), (test_images,test_labels)
 
-        return new
+    def run(self, epochs=10, train=True, evaluate=True, plot=True, data_prep=True):
+        if train:
+            (train_images, train_labels), (test_images, test_labels) = self.load_data()
 
-    def data_prep(self, resize_factor):
-        self.resize_factor = resize_factor
+            train_images = train_images/255.0
+            test_images = test_images/255.0
 
-        img = mpimg.imread('images/train_image1.jpg')[:,:,:-1]
-        np_im1 = self.resize(img)
+            self.model = Sequential()
+            self.model.add(Conv2D(32, (5,5), activation='relu', input_shape=(self.new_height,self.new_width,3)))
+            self.model.add(MaxPooling2D(pool_size=(2,2)))
+            self.model.add(Conv2D(32, (5,5), activation='relu'))
+            self.model.add(MaxPooling2D(pool_size=(2,2)))
+            self.model.add(Flatten())
+            self.model.add(Dense(1024, activation='relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(512, activation='relu'))
+            self.model.add(Dropout(0.5))
+            self.model.add(Dense(256, activation='relu'))
+            self.model.add(Dense(2, activation='softmax'))
 
-        img = mpimg.imread('images/train_image2.jpg')[:,:,:-1]
-        np_im2 = self.resize(img)
+            start_time = time.time()
+            self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+            hist = self.model.fit(train_images, train_labels, epochs=epochs)
+            end_time = time.time() - start_time
 
-        img = mpimg.imread('images/test_image1.jpg')[:,:,:-1]
-        np_im3 = self.resize(img)
+            if end_time > 3600:
+                print('Total Training Time: %shrs\n\n' % round((end_time/3600),2))
+            elif end_time > 60:
+                print("Total Training Time: %smins\n\n" % round((end_time/60),2))
+            else:
+                print("Total Training Time: %ss\n\n" % round(end_time,2))
 
-        img = mpimg.imread('images/test_image2.jpg')[:,:,:-1]
-        np_im4 = self.resize(img)
+        if evaluate:
+            print("TESTING")
+            self.model.evaluate(test_images, test_labels)[1]
+            print('\n')
 
-        self.train_images = np.array([np_im1, np_im2])/255.0
-        self.train_labels = np.array([[0],[1]])
-        self.test_images = np.array([np_im3, np_im4])/255.0
-        self.test_labels = np.array([[0],[1]])
-
-        self.train_labels_one_hot = to_categorical(self.train_labels)
-        self.test_labels_one_hot = to_categorical(self.test_labels)
-
-        # f = plt.figure()
-        # f.add_subplot(2,1, 1)
-        # plt.imshow(self.np_im1.astype('uint8'))
-        # f.add_subplot(2,1, 2)
-        # plt.imshow(self.np_im2.astype('uint8'))
-        # plt.show()
-
-    def run(self, epochs):
-        self.model = Sequential()
-
-        self.model.add(Conv2D(32, (5,5), activation='relu', input_shape=(300,480,3)))
-        self.model.add(MaxPooling2D(pool_size=(2,2)))
-        self.model.add(Conv2D(32, (5,5), activation='relu'))
-        self.model.add(MaxPooling2D(pool_size=(2,2)))
-        self.model.add(Flatten())
-        self.model.add(Dense(1024, activation='relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(512, activation='relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(256, activation='relu'))
-        self.model.add(Dense(2, activation='softmax'))
-
-        start_time = time.time()
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        hist = self.model.fit(self.train_images, self.train_labels_one_hot, epochs=epochs)
-        end_time = time.time() - start_time
-
-        if end_time > 60:
-            print("Total Training Time: %smins\n\n" % round((end_time/60),2))
-        else:
-            print("Total Training Time: %ss\n\n" % round(end_time,2))
-
-        self.model.evaluate(self.test_images, self.test_labels_one_hot)[1]
-        print('\n')
-
-        # plt.plot(hist.history['accuracy'])
-        # plt.title('Model Accuracy')
-        # plt.xlabel('Epoch')
-        # plt.ylabel('Accuracy')
-        # plt.show()
-
-        # plt.plot(hist.history['loss'])
-        # plt.title('Model Loss')
-        # plt.xlabel('Epoch')
-        # plt.ylabel('Loss')
-        # plt.show()
+        if plot:
+            f = plt.figure()
+            f.add_subplot(2,1, 1)
+            plt.plot(hist.history['accuracy'])
+            plt.title('Model Accuracy (top) and Model Loss (bottom)')
+            plt.ylabel('Accuracy')
+            f.add_subplot(2,1, 2)
+            plt.plot(hist.history['loss'])
+            plt.ylabel('Loss')
+            plt.xlabel('Epoch')
+            plt.show()
