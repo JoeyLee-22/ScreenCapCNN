f74fc7bf6f5faa8a52e404e8e4a00a5767b3122b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 21:28:15 -0700

    add get_data

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from CNN import convolutional_neural_network
 (new_height,new_width) = (265, 420)
 
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=50, train=True, plot=False, data_prep=True)
+cnn.run(epochs=50, train=True, plot=False, data_prep=True, get_data=True)
 
 start(cnn, new_height, new_width, minutes=0.1)
