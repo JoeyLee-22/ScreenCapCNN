63af05c9b1f33651e56bf2462043a12cc9bebc6b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-10 09:06:19 -0700

    update name to my_resize

--- screen_cap.py
+++ screen_cap.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 import cv2
 import pyautogui
-from resize import resize
+from resize import my_resize
 
 def start(cnn_instance, new_height, new_width, minutes=5):
     class_names = ["Good", "Bad"]
@@ -10,7 +10,7 @@ def start(cnn_instance, new_height, new_width, minutes=5):
     while(True):
         og_image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
 
-        result = cnn_instance.classify(np.array([resize(og_image, new_height, new_width)]))
+        result = cnn_instance.classify(np.array([my_resize(og_image, new_height, new_width)]))
 
         # if result == 0:
         #     cv2.imwrite("images/good.jpg", og_image)
