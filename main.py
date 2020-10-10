04adffe950f8ce23f5d771b7a838d62d58a9264b
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-09 17:04:46 -0700

    change new_height and new_width

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from screen_cap import start
 from CNN import convolutional_neural_network
 
-(new_height,new_width) = (300,480)
+(new_height,new_width) = (265, 420)
 
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=50, train=True, plot=False, data_prep=False)
+cnn.run(epochs=50, train=True, plot=False, data_prep=True)
 
 start(cnn, new_height, new_width, minutes=0.1)
