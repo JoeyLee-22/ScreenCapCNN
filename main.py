f9898437379ee2c1f7aca34371864baeacb3b351
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-12 08:36:46 -0700

    update .gitignore

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from CNN import convolutional_neural_network
 (new_height,new_width) = (265, 420)
 
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=50, train=True, plot=False, data_prep=False, clear_data=False)
+cnn.run(epochs=50, train=True, evaluate=True, plot=False, data_prep=True, clear_data=True)
 
 start(cnn, new_height, new_width, minutes=0.1)
