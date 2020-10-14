ba3171d49227365144e7fc85591e515569b0f4c2
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-14 11:15:51 -0700

    add load model option

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from screen_cap import start
 from CNN import convolutional_neural_network
 
-(new_height,new_width) = (350, 560) 
+(new_height,new_width) = (300, 480) 
 
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=50, train=True, evaluate=True, plot=False, data_prep=True, clear_data=True)
+cnn.run(epochs=50, load_model=True, train=True, evaluate=True, plot=False, data_prep=True, clear_data=False)
 
 start(cnn, new_height, new_width, minutes=0.1)
