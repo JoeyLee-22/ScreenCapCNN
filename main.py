a4fbaff0e2f78a3c764077677d7f62c094ce7e47
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-21 13:11:02 -0700

    change run parameters

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from CNN import convolutional_neural_network
 (new_height,new_width) = (300, 480) 
 
 cnn = convolutional_neural_network(new_height, new_width)
-cnn.run(epochs=50, load_model=False, save_model=True, train=True, evaluate=True, plot=False, data_prep=True, clear_data=True)
+cnn.run(epochs=50, load_model=False, save_model=True, train=True, evaluate=True, plot=True, data_prep=True, clear_data=True)
 
 start(cnn, new_height, new_width, minutes=0.1)
