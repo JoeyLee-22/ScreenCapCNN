e78be83ee0c8fdde855ea14dc253014faf8802c5
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-27 19:27:56 -0700

    take out new width and new height arguments

--- screen_cap.py
+++ screen_cap.py
@@ -4,12 +4,12 @@ import cv2
 import pyautogui
 from resize import my_resize
 
-def start(cnn_instance, new_height, new_width, minutes=5):
+def start(cnn_instance, minutes=5):
     class_names = ["Productive", "Nonproductive"]
 
     while(True):
         og_image = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
-        result = cnn_instance.classify(np.array([my_resize(og_image, new_height, new_width)]))
+        result = cnn_instance.classify(np.array([my_resize(og_image, cnn_instance.new_height, cnn_instance.new_width)]))
 
         if result == 0:
             cv2.imwrite("screenshots/Productive.jpg", og_image)
