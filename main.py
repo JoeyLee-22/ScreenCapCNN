02757490aa2c3d01126c8e46ef98599c9a2b17b0
Joey Lee <66647435+JoeyLee-22@users.noreply.github.com>
2020-10-30 21:03:00 -0700

    change resize dimensions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from screen_cap import start
 from CNN import convolutional_neural_network
 
 #screen size = (2100, 3360)
-cnn = convolutional_neural_network(300, 480)
+cnn = convolutional_neural_network(350, 560)
 cnn.run(epochs=50, load_model=False, save_model=True, train=True, evaluate=True, plot=True, data_prep=True, clear_data=True)
 
 start(cnn, minutes=0.1)
